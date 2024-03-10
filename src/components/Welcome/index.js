848ab7fc779ac04d8d2a6b81f2f6a62ca7ca0013
Narendra <monenarendrakumar@gmail.com>
2024-03-10 09:34:59 +0000

    added files

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,30 @@
+import {Component} from 'react'
+import './index.css'
+
+class Welcome extends Component {
+  state = {isSubscribed: false}
+
+  onSub = () => {
+    this.setState(prevState => ({
+      isSubscribed: !prevState.isSubscribed,
+    }))
+  }
+
+  render() {
+    const {isSubscribed} = this.state
+
+    return (
+      <div className="bg-container">
+        <div>
+          <h1 className="main-heading">Welcome</h1>
+          <p className="discription">Thanks you!Happy Learning</p>
+          <button type="button" className="btn" onClick={this.onSub}>
+            {isSubscribed ? 'Subscribed' : 'Subscrib'}
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Welcome
